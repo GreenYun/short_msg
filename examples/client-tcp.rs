ee2053e303f0644e04df4bfe29964ed678928594
GreenYun <microsoft.zh.cn@gmail.com>
2022-09-13 11:06:52 +0000

    1663067212

--- examples/client-tcp.rs
+++ examples/client-tcp.rs
@@ -89,17 +89,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
             stream.write_all(&unbind).await?;
 
-            stream.shutdown().await?;
+            let data_len = stream.read_u32().await?;
 
-            // let data_len = stream.read_u32().await?;
+            let mut buf = BytesMut::with_capacity(data_len as usize);
+            stream.read_buf(&mut buf).await?;
 
-            // let mut buf = BytesMut::with_capacity(data_len as usize);
-            // stream.read_buf(&mut buf).await?;
-
-            // let mut resp = buf.to_vec();
-            // resp.splice(0..0, bincode::encode_to_vec(data_len, config)?);
-            // let (resp, _) = bincode::decode_from_slice::<Header, _>(&resp,
-            // config)?; println!("{:?}", resp);
+            let mut resp = buf.to_vec();
+            resp.splice(0..0, bincode::encode_to_vec(data_len, config)?);
+            let (resp, _) = bincode::decode_from_slice::<Header, _>(&resp, config)?;
+            println!("{:?}", resp);
         }
     }
 
